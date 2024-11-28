cd382bfccb4e3f597ada20dc186836cc6e6a6601
RCPilot1604 <ethan.chua1604@gmail.com>
2024-11-28 16:59:39 +0800

    Revert "Add OTA code"
    This reverts commit 917316eca735c892ce6f560d217807c2450b0659.

--- Player Piano/src/main.cpp
+++ Player Piano/src/main.cpp
@@ -7,11 +7,6 @@
 #include <vector>
 #include <iterator>
 
-//For OTA
-#include <WiFi.h>
-#include <ESPmDNS.h>
-#include <ArduinoOTA.h>
-
 #include "Note.h"
 #include "Display.h"
 #include "Settings.h"
@@ -331,52 +326,7 @@ void OnNoteOff(uint8_t channel, uint8_t note, uint8_t velocity) {
 
 void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(mySettings.ssid, mySettings.password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
-        type = "sketch";
-      } else {  // U_SPIFFS
-        type = "filesystem";
-      }
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) {
-        Serial.println("Auth Failed");
-      } else if (error == OTA_BEGIN_ERROR) {
-        Serial.println("Begin Failed");
-      } else if (error == OTA_CONNECT_ERROR) {
-        Serial.println("Connect Failed");
-      } else if (error == OTA_RECEIVE_ERROR) {
-        Serial.println("Receive Failed");
-      } else if (error == OTA_END_ERROR) {
-        Serial.println("End Failed");
-      }
-    });
-
-  ArduinoOTA.begin();
-
   Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
   //Setup I2C
   Wire.begin(21,22);
   
@@ -491,7 +441,6 @@ void setup() {
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  ArduinoOTA.handle(); //Arduino OTA
   MIDI.read();
   counter = 0;                                              //reset the counter
   for (auto it = notes.begin(); it != notes.end(); it++) {  //iterate through the vector of notes
